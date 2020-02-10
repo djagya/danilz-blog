dd129c6e3f131dbd835f7a0a41e4058068b5550f
djagya <djagya@fastmail.com>
2020-02-10 21:28:39 +0100

    configure styles for preview pane of the CMS

---
+++ src/cms/cms.js
@@ -0,0 +1,12 @@
+/**
+ * The default export of `netlify-cms-app` is an object with all of the Netlify CMS
+ * extension registration methods, such as `registerWidget` and
+ * `registerPreviewTemplate`.
+ */
+/**
+ * Any imported styles will automatically be applied to the editor preview
+ * pane, there is no need to use `registerPreviewStyle` for imported styles.
+ * All of the example imports below would result in styles being applied to the
+ * preview pane.
+ */
+import "../utils/typography"
